5c9bd4e96fdc28bdf761ffe77f5a61455a6231b5
Kabir Sarin <sarink87@gmail.com>
2013-02-25 19:50:03 -0600

    Update lib/generators/backbone/scaffold/scaffold_generator.rb

--- lib/generators/backbone/scaffold/scaffold_generator.rb
+++ lib/generators/backbone/scaffold/scaffold_generator.rb
@@ -14,12 +14,14 @@ module Backbone
       
       def create_view_files
         available_views.each do |view|
-          template "views/#{view}_view.coffee", File.join(backbone_path, "views", "#{plural_name}/coffee", "#{view}_view.js.coffee")
+          #template "views/#{view}_view.coffee", File.join(backbone_path, "views", "#{plural_name}/coffee", "#{view}_view.js.coffee")
+          template "views/#{view}_view.coffee", File.join(backbone_path, "views/coffee", plural_name, "#{view}_view.js.coffee")
           template "views/#{view}_view.javascript", File.join(backbone_path, "views", plural_name, "#{view}_view.js")
           template "templates/#{view}.jst", File.join(backbone_path, "templates", plural_name, "#{view}.jst.ejs")       
         end
         
-        template "views/model_view.coffee", File.join(backbone_path, "views", "#{plural_name}/coffee", "#{singular_name}_view.js.coffee")
+        #template "views/model_view.coffee", File.join(backbone_path, "views", "#{plural_name}/coffee", "#{singular_name}_view.js.coffee")
+        template "views/model_view.coffee", File.join(backbone_path, "views/coffee", plural_name, "#{singular_name}_view.js.coffee")
         template "views/model_view.javascript", File.join(backbone_path, "views", plural_name, "#{singular_name}_view.js")
         template "templates/model.jst", File.join(backbone_path, "templates", plural_name, "#{singular_name}.jst.ejs") 
       end
